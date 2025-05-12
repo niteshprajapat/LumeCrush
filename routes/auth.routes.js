6f1c296f80b93c9fda74a8d58a5c144dde210da9
Nitesh Prajapat <niteshp282000@gmail.com>
2025-05-13 00:09:46 +0530

    create API routes for login and verify account

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, register } from '../controllers/auth.controller.js';
+import { login, register, verifyAccount } from '../controllers/auth.controller.js';
 
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/verify-account", verifyAccount);
 
 
 export default router;
