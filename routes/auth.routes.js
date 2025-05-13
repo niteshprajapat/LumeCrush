db6ce6c9927c78edb306156a90948ba3fc0ce9ca
Nitesh Prajapat <niteshp282000@gmail.com>
2025-05-13 22:13:15 +0530

    created API routes  or resent token, forgot password, verify account

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, register, verifyAccount } from '../controllers/auth.controller.js';
+import { forgotPassword, login, register, resendToken, verifyAccount } from '../controllers/auth.controller.js';
 
 
 const router = express.Router();
@@ -8,7 +8,12 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+// logout
 router.post("/verify-account", verifyAccount);
+router.post("/resend-token", resendToken);
+router.post("/forgot-password", forgotPassword);
+// router.post("/resend-token", resetPassword);
+
 
 
 export default router;
