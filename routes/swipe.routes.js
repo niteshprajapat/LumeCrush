64831fe6dd3bc8fdfec9c2d82904c6e4658547d1
Nitesh Prajapat <niteshp282000@gmail.com>
2025-05-17 09:51:01 +0530

    create API route for swipe and match making algo

---
+++ routes/swipe.routes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { isAuthenticated } from '../middlewares/authMiddleware.js';
+import { swipeUser } from '../controllers/swipe.controller.js';
+
+
+const router = express.Router();
+
+
+
+router.post("/swipeUser", isAuthenticated, swipeUser);
+// router.get("/get-swipe-history", isAuthenticated, getSwipeHistoryOfUser);
+
+
+
+
+export default router;
+
