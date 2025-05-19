5419601010960a8ef6d8e19b01fb8b49976d4f83
Nitesh Prajapat <niteshp282000@gmail.com>
2025-05-19 23:57:03 +0530

    created API controller for deleteMessageById

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -52,12 +52,89 @@ export const getMessagesByMatchId = async (req, res) => {
         const matchId = req.params.matchId;
         const senderId = req.user._id;
 
-        const messages = await Message.find({ matchId: matchId });
+        const messages = await Message.find({ matchId: matchId, isDeleted: false });
 
         return res.status(200).json({
             success: true,
             message: "Fetched Messages using Match Id Successfully!",
             messages,
+        });
+
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error in getMessagesByMatchId API!"
+        })
+    }
+}
+
+// deleteMessagesBymessageId
+export const deleteMessagesBymessageId = async (req, res) => {
+    try {
+        const messageId = req.params.messageId;
+        const senderId = req.user._id;
+
+        const message = await Message.findById(messageId);
+        if (message.isDeleted) {
+            return res.status(400).json({
+                success: false,
+                message: "Message Already Deleted! Message Not Found!"
+            });
+        }
+
+        if (message.sender.toString() !== senderId.toString()) {
+            return res.status(400).json({
+                success: false,
+                message: "You can't delete this message! You can only delete your own message!",
+            });
+        }
+
+        message.isDeleted = true;
+        await message.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Message Deleted Successfully!",
+        });
+
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error in getMessagesByMatchId API!"
+        })
+    }
+}
+
+
+// updateMessagesBymessageId
+export const updateMessagesBymessageId = async (req, res) => {
+    try {
+        const messageId = req.params.messageId;
+        const senderId = req.user._id;
+
+        const message = await Message.findById(messageId);
+        if (message.isDeleted) {
+            return res.status(400).json({
+                success: false,
+                message: "Message Already Deleted! Message Not Found!"
+            });
+        }
+
+        if (message.sender.toString() !== senderId.toString()) {
+            return res.status(400).json({
+                success: false,
+                message: "You can't delete this message! You can only delete your own message!",
+            });
+        }
+
+        message.isDeleted = true;
+        await message.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Message Deleted Successfully!",
         });
 
     } catch (error) {
