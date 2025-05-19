bc2e2cf13070a6ff75c86aaf33185782d17cd8b7
Nitesh Prajapat <niteshp282000@gmail.com>
2025-05-19 23:56:30 +0530

    created API routes for updateMessage and deleteMessage by Ids

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { getMessagesByMatchId, sendMessage } from '../controllers/message.controller.js';
+import { getMessagesByMatchId, sendMessage, deleteMessagesBymessageId, updateMessagesBymessageId } from '../controllers/message.controller.js';
 
 
 const router = express.Router();
@@ -9,6 +9,8 @@ const router = express.Router();
 
 router.post("/send-message", isAuthenticated, sendMessage);
 router.get("/get-messages-by-matchId/:matchId", isAuthenticated, getMessagesByMatchId);
+router.put("/update-messages/:messageId", isAuthenticated, updateMessagesBymessageId);
+router.delete("/delete-messages/:messageId", isAuthenticated, deleteMessagesBymessageId);
 
 
 
