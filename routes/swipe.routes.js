434e63d4d77c9efdd4845a3440ab2ba3c0b76161
Nitesh Prajapat <niteshp282000@gmail.com>
2025-05-24 12:45:37 +0530

    created getLikesReceived API route to fetch list

--- routes/swipe.routes.js
+++ routes/swipe.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { getSwipeHistoryOfUser, swipeUser } from '../controllers/swipe.controller.js';
+import { getLikesReceived, getSwipeHistoryOfUser, swipeUser } from '../controllers/swipe.controller.js';
 
 
 const router = express.Router();
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.post("/swipeUser", isAuthenticated, swipeUser);
 router.get("/get-swipe-history", isAuthenticated, getSwipeHistoryOfUser);
+router.get("/get-likes-received", isAuthenticated, getLikesReceived);
 
 
 
