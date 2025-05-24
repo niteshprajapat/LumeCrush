b88edda7c443db14d1c222d0ac71e1de9c3ec0d5
Nitesh Prajapat <niteshp282000@gmail.com>
2025-05-24 18:02:49 +0530

    created API controller for user profile, update preference, proflebyid

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,10 @@ import { logger } from "../utils/logger.js"
 
 
 
+// Use this api to get latitude and longitude using city name
+// https://api.openweathermap.org/data/2.5/weather?q=jodhpur&appid=0318c1563a65f62cbdd1f39c1d54301a
+
+
 // getDiscoverUsers
 export const getDiscoverUsers = async (req, res) => {
     try {
@@ -62,11 +66,125 @@ export const getDiscoverUsers = async (req, res) => {
 
 
         return res.status(200).json({
+
             success: true,
             message: `Fetched discoveries Successfully!`,
             users,
             total: users.length,
         });
+
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error in getDiscoverUsers API!"
+        })
+    }
+}
+
+// meProfile
+export const meProfile = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const user = await User.findById(userId);
+        if (!user || user.isDeleted) {
+            return res.status(404).json({
+                success: false,
+                message: "User Not Found!",
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Fetched LoggedIn User Profile",
+            user,
+        });
+
+
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error in getDiscoverUsers API!"
+        })
+    }
+}
+
+// userProfileByUserId
+export const userProfileByUserId = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+
+        const user = await User.findById(userId);
+        if (!user || user.isDeleted) {
+            return res.status(404).json({
+                success: false,
+                message: "User Not Found!",
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Fetched User Profile by Id",
+            user,
+        });
+
+
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error in getDiscoverUsers API!"
+        })
+    }
+}
+
+// updatePreferences
+export const updatePreferences = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const { minAge, maxAge, maxDistance, gender } = req.body;
+
+        const user = await User.findById(userId);
+        if (!user || user.isDeleted) {
+            return res.status(404).json({
+                success: false,
+                message: "User Not Found!",
+            });
+        }
+
+        if (minAge < 18) {
+            return res.status(400).json({
+                success: false,
+                message: "Min age should atleast 18!",
+            });
+        }
+
+        if (minAge) {
+            user.preferences.minAge = Number(minAge);
+        }
+
+        if (maxAge) {
+            user.preferences.maxAge = Number(maxAge);
+        }
+
+        if (maxDistance) {
+            user.preferences.maxDistance = Number(maxDistance);
+        }
+
+        if (gender) {
+            user.preferences.gender = gender;
+        }
+
+        await user.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Preferences Updated Successfully!",
+            user,
+        })
+
 
     } catch (error) {
         logger.error(error);
