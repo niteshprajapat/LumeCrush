1195c3aeac96cfc97f89ff60d0a9df6fc38c6b2e
Nitesh Prajapat <niteshp282000@gmail.com>
2025-05-24 18:02:29 +0530

    created API routes for user profile, update preference, proflebyid

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { getDiscoverUsers } from '../controllers/user.controller.js';
+import { getDiscoverUsers, meProfile, updatePreferences, userProfileByUserId } from '../controllers/user.controller.js';
 
 
 const router = express.Router();
@@ -8,6 +8,11 @@ const router = express.Router();
 
 
 router.get("/discover", isAuthenticated, getDiscoverUsers);
+
+router.get("/me-profile", isAuthenticated, meProfile);
+router.get("/user-profile/:userId", isAuthenticated, userProfileByUserId);
+router.put("/update-preferences", isAuthenticated, updatePreferences);
+// router.put("/update-profile", isAuthenticated, updateProfile);
 
 
 
