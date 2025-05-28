8fc9a2daad30d80b9118ff72874e50960c41cdba
Nitesh Prajapat <niteshp282000@gmail.com>
2025-05-28 23:54:16 +0530

    created API route for createCheckoutSession

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { getDiscoverUsers, meProfile, updatePreferences, userProfileByUserId } from '../controllers/user.controller.js';
+import { createCheckoutSession, getDiscoverUsers, meProfile, updatePreferences, userProfileByUserId } from '../controllers/user.controller.js';
 
 
 const router = express.Router();
@@ -13,6 +13,8 @@ router.get("/me-profile", isAuthenticated, meProfile);
 router.get("/user-profile/:userId", isAuthenticated, userProfileByUserId);
 router.put("/update-preferences", isAuthenticated, updatePreferences);
 // router.put("/update-profile", isAuthenticated, updateProfile);
+
+router.post("/create-checkout-session", isAuthenticated, createCheckoutSession);
 
 
 
