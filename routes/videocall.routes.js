900225867a00606a53945e65be9bd1c9157f48e3
Nitesh Prajapat <niteshp282000@gmail.com>
2025-06-10 21:43:22 +0530

    created API routes for video calls

--- routes/videocall.routes.js
+++ routes/videocall.routes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { endVideoCall, startVideoCall } from "../controllers/videocall.controller.js";
+import { endVideoCall, getAllVideoCalls, startRecording, startVideoCall, stopRecording } from "../controllers/videocall.controller.js";
 
 
 const router = express.Router();
 
 router.post("/start-video-call", isAuthenticated, startVideoCall);
 router.post("/end-video-call", isAuthenticated, endVideoCall);
-
+router.post("/start-recording", isAuthenticated, startRecording);
+router.post("/stop-recording", isAuthenticated, stopRecording);
+router.get("/get-all-videocalls", isAuthenticated, getAllVideoCalls);
 
 export default router;
