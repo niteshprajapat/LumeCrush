bdd25980894c2404c56c7ac977016a9e78bf7417
Nitesh Prajapat <niteshp282000@gmail.com>
2025-06-18 22:41:08 +0530

    created API route for deletePost

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
 import upload from '../middlewares/multer.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, deletePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 
 router.post("/create-post", isAuthenticated, upload.single("image"), createPost);
+router.delete("/delete-post/:postId", isAuthenticated, deletePost);
 
 export default router;
