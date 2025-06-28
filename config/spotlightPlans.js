2b5e8f35d98f10873a88184e2d1bfd7f38b8013a
Nitesh Prajapat <niteshp282000@gmail.com>
2025-06-28 22:41:17 +0530

    created plans for spotlight

---
+++ config/spotlightPlans.js
@@ -0,0 +1,32 @@
+
+
+export const spotlightPlans = [
+    {
+        name: "30 Spotlights",
+        quantity: 30,
+        pricePerSpotlight: 30,
+        totalAmount: 900,
+        stripePriceId: "price_1RexY5J49BwuBznRJyUZfSeI",
+    },
+    {
+        name: "15 Spotlights",
+        quantity: 15,
+        pricePerSpotlight: 35,
+        totalAmount: 525,
+        stripePriceId: "price_1RexZ1J49BwuBznRf0nZaJXp",
+    },
+    {
+        name: "5 Spotlights",
+        quantity: 5,
+        pricePerSpotlight: 40,
+        totalAmount: 200,
+        stripePriceId: "price_1Rexa4J49BwuBznRLaqrhU20",
+    },
+    {
+        name: "1 Spotlight",
+        quantity: 1,
+        pricePerSpotlight: 60,
+        totalAmount: 60,
+        stripePriceId: "price_1RexaoJ49BwuBznRp9wZIHHg",
+    },
+]
