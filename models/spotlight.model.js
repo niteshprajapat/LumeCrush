71c30624c50248039f716dd9eee9da7cf56c18b5
Nitesh Prajapat <niteshp282000@gmail.com>
2025-06-28 22:42:11 +0530

    updated spotlight model

--- models/spotlight.model.js
+++ models/spotlight.model.js
@@ -8,15 +8,46 @@ const spotlightSchema = new mongoose.Schema({
         index: true,
     },
     quantity: {
-        type: Number
-    }
+        type: Number,
+        required: true,
+    },
+    pricePerSpotlight: {
+        type: Number,
+        required: true,
+    },
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+    stripePaymentIntentId: {
+        type: String,
+        required: true,
+    },
+    purchaseDate: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        enum: ['completed', 'refunded'],
+        default: "completed",
+    },
+    isActive: {
+        type: Boolean,
+    },
+    startTime: {
+        type: Date,
+    },
+    endTime: {
+        type: Date,
+    },
 
 
 
 }, { timestamps: true });
 
-
-swipeSchema.index({ swiper: 1, target: 1 }, { unique: true });
+// Index for efficient querying of active spotlights
+spotlightSchema.index({ isActive: 1, endTime: 1 });
 
 const Spotlight = mongoose.model("Spotlight", spotlightSchema);
 export default Spotlight;
