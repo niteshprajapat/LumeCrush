a2994df12244dd5a7417d3db1839edf793454e06
Nitesh Prajapat <niteshp282000@gmail.com>
2025-07-06 23:24:36 +0530

    created API routes for getSpotlights

--- routes/spotlight.routes.js
+++ routes/spotlight.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { activateSpotlight, purchaseSpotlight } from '../controllers/spotlight.controller.js';
+import { activateSpotlight, getSpotlights, purchaseSpotlight } from '../controllers/spotlight.controller.js';
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.post("/purchase-spotlight", isAuthenticated, purchaseSpotlight);
 router.post("/activate-spotlight", isAuthenticated, activateSpotlight);
+router.get("/get-spotlight", isAuthenticated, getSpotlights);
 
 export default router;
 
